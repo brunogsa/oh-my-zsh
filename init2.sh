914b3706944f5d991cb083dade1d55badc148d43
Bruno Agostini <brunogsa.92@gmail.com>
2017-11-19 21:40:18 -0200

    Rename config.sh -> init2.sh

--- init2.sh
+++ init2.sh
@@ -11,20 +11,21 @@ sudo ln -s ~/oh-my-zsh/.zshrc /root/.zshrc
 
 # Themes
 wget https://raw.githubusercontent.com/dannynimmo/punctual-zsh-theme/v0.1.0/punctual.zsh-theme
+rm -f ~/.oh-my-zsh/themes/*
 mv -f punctual.zsh-theme ~/.oh-my-zsh/themes/
 
 # Plugin
 git clone https://github.com/akoenig/npm-run.plugin.zsh
-sudo mv -f npm-run.plugin.zsh ~/.oh-my-zsh/custom/plugins/
+mv -f npm-run.plugin.zsh ~/.oh-my-zsh/custom/plugins/
 
 git clone https://github.com/lukechilds/zsh-better-npm-completion
-sudo mv -f zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/
+mv -f zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/
 
 git clone https://github.com/junegunn/fzf.git
-sudo mv -f fzf ~/.oh-my-zsh/custom/plugins/
+mv -f fzf ~/.oh-my-zsh/custom/plugins/
 ~/.oh-my-zsh/custom/plugins/fzf/install --bin
 
 git clone https://github.com/Treri/fzf-zsh.git
-sudo mv -f fzf-zsh ~/.oh-my-zsh/custom/plugins/
+mv -f fzf-zsh ~/.oh-my-zsh/custom/plugins/
 
 echo "Done!"
