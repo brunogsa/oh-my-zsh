2833b49839e3c9e601c508fba0c9fcb7fd483d70
Bruno Agostini <brunogsa.92@gmail.com>
2018-01-17 18:52:45 -0200

    Fix the installation process for oh-my-zsh

--- init.sh
+++ init.sh
@@ -9,7 +9,3 @@ sudo apt install -y zsh
 wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 sudo chmod +x install.sh
 ./install.sh
-sudo rm -f install.sh
-sudo chsh -s $(which zsh)
-chsh -s $(which zsh)
-sudo reboot
--- init2.sh
+++ init2.sh
@@ -1,34 +1,7 @@
 #!/bin/bash
 set -e
 
-# Use my configs
-rm -f ~/.zshrc
-sudo rm -f /root/.zshrc
-sudo rm -fr /root/.oh-my-zsh
-
-ln -s ~/oh-my-zsh/.zshrc ~/.zshrc
-sudo ln -s ~/.oh-my-zsh /root/.oh-my-zsh
-sudo ln -s ~/oh-my-zsh/.zshrc /root/.zshrc
-
-# Themes
-wget https://raw.githubusercontent.com/dannynimmo/punctual-zsh-theme/v0.1.0/punctual.zsh-theme
-rm -f ~/.oh-my-zsh/themes/*
-mv -f punctual.zsh-theme ~/.oh-my-zsh/themes/
-
-# Plugin
-sudo rm -fr ~/.oh-my-zsh/custom/plugins/*
-
-git clone https://github.com/akoenig/npm-run.plugin.zsh
-mv -f npm-run.plugin.zsh ~/.oh-my-zsh/custom/plugins/
-
-git clone https://github.com/lukechilds/zsh-better-npm-completion
-mv -f zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/
-
-git clone https://github.com/junegunn/fzf.git
-mv -f fzf ~/.oh-my-zsh/custom/plugins/
-~/.oh-my-zsh/custom/plugins/fzf/install --bin
-
-git clone https://github.com/Treri/fzf-zsh.git
-mv -f fzf-zsh ~/.oh-my-zsh/custom/plugins/
-
-echo "Done!"
+sudo rm -f install.sh
+sudo chsh -s $(which zsh)
+chsh -s $(which zsh)
+sudo reboot
