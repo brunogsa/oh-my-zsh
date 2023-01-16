235250824e02cd9af6d929bde1f852354faec838
brunogsa <brunogsa.92@gmail.com>
2023-01-16 14:38:12 -0300

    Fix installation for mac

--- init3.sh
+++ init3.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -e
 
-# Use my configs
-rm -f ~/.zshrc
-sudo rm -f /root/.zshrc
-sudo rm -fr /root/.oh-my-zsh
+# Dependency on mac
+brew install coreutils
 
-ln -s ~/oh-my-zsh/.zshrc ~/.zshrc
-sudo ln -s ~/.oh-my-zsh /root/.oh-my-zsh
-sudo ln -s ~/oh-my-zsh/.zshrc /root/.zshrc
+ln -sf ~/oh-my-zsh/.zshrc ~/.zshrc
+sudo ln -sf ~/.oh-my-zsh /Users/admin/.oh-my-zsh
+sudo ln -sf ~/oh-my-zsh/.zshrc /Users/admin/.zshrc
 
 # Themes
 wget https://raw.githubusercontent.com/dannynimmo/punctual-zsh-theme/v0.1.0/punctual.zsh-theme
@@ -19,8 +17,6 @@ sudo rm -fr ~/.oh-my-zsh/custom/plugins/*
 
 git clone https://github.com/lukechilds/zsh-better-npm-completion
 mv -f zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/
-
-sudo apt-get install silversearcher-ag -y
 
 git clone https://github.com/junegunn/fzf.git
 mv -f fzf ~/.oh-my-zsh/custom/plugins/
